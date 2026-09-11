8c809deade03e83aa1a10251206c51e84ca14fe0
Valentina Sanchez-Patout <51valentina91@gmail.com>
2026-09-11 10:22:28 -0400

    Created sonnet class and readme

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "Prithee"
+include("docs")
---
+++ src/main/java/Sonnet.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+public class Sonnet {
+
+    private final String[] words;
+    private final boolean[] used;
+    private final Random random = new Random();
+
+    public Sonnet(String text) {
+        this.words = text.replace("\n", " ").trim().split(" ");
+        this.used = new boolean[words.length];
+    }
+
+    public int wordCount() {
+        return words.length;
+    }
+
+    public String wordAt(int index) {
+        return words[index];
+    }
+
+    // picks a random word that hasn't been blanked yet
+    public int pickUnusedIndex() {
+        int index;
+        do {
+            index = random.nextInt(words.length);
+        } while (used[index]);
+        used[index] = true;
+        return index;
+    }
+
+    // prints everything up to the blank, then underscores, then stops
+    public String renderUpTo(int blankIndex) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= blankIndex; i++) {
+            if (i == blankIndex) {
+                sb.append("_".repeat(words[i].length()));
+            } else {
+                sb.append(words[i]).append(" ");
+            }
+        }
+        return sb.toString();
+    }
+}
